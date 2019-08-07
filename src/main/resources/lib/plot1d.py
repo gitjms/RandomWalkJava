c394a7a63a9be712d45dca4f038964900da66fc8
gitjms <46410240+gitjms@users.noreply.github.com>
2019-08-07 10:02:49 +0300

    lib koodit mukaan

---
+++ src/main/resources/lib/plot1d.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+	path_trace_x = sys.argv[1]
+
+#!----------------------------------------------------------------------
+#!	DATA
+#!----------------------------------------------------------------------
+	xdata_path = np.genfromtxt(path_trace_x, dtype='float',skip_header=1)
+
+	f = open(path_trace_x,'r')
+	facts = f.readline().split()
+	f.close()
+
+	particles = int(facts[3].split('=')[1].replace(',',''))
+	steps = int(facts[4].split('=')[1])
+
+	minx = np.min(xdata_path)
+	maxx = np.max(xdata_path)
+
+	y = np.ones(steps+1)
+
+#!----------------------------------------------------------------------
+#!	PLOT
+#!----------------------------------------------------------------------
+#	kuvan koko
+	fig = plt.figure()
+	fig.set_figheight(7)
+	fig.set_figwidth(9)
+	plt.gca().ticklabel_format(axis='both', style='plain', useOffset=False)
+
+	plt.plot(xdata_path,y,'-',lw=8,alpha=0.2,label="Path trace, N=%d"%int(particles))
+	plt.xlim(minx-10,maxx+10)
+	plt.ylim(0,2)
+	
+	plt.tight_layout()
+	savename = "jpyplot1D_N" + str(particles) + "_S" + str(steps) + ".png"
+	plt.savefig(savename)
+
+if __name__=="__main__":
+	main()
---
+++ src/main/resources/lib/plot2d.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+	path_trace_x = sys.argv[1]
+	path_trace_y = sys.argv[2]
+#!----------------------------------------------------------------------
+#!	DATA
+#!----------------------------------------------------------------------
+	xdata_path = np.genfromtxt(path_trace_x, dtype='float',skip_header=1)
+	ydata_path = np.genfromtxt(path_trace_y, dtype='float',skip_header=1)
+
+	f = open(path_trace_x,'r')
+	facts = f.readline().split()
+	f.close()
+
+	particles = int(facts[3].split('=')[1].replace(',',''))
+	steps = int(facts[4].split('=')[1])
+
+	minx = np.min(xdata_path)
+	maxx = np.max(xdata_path)
+	miny = np.min(ydata_path)
+	maxy = np.max(ydata_path)
+
+#!----------------------------------------------------------------------
+#!	PLOT
+#!----------------------------------------------------------------------
+	#! kuvan koko
+	fig = plt.figure()
+	fig.set_figheight(7)
+	fig.set_figwidth(9)
+	plt.gca().ticklabel_format(axis='both', style='plain', useOffset=False)
+	
+	plt.plot(xdata_path,ydata_path,'-',lw=0.5,label="Path trace, N=%d"%int(particles))
+
+	plt.xlim(minx-10,maxx+10)
+	plt.ylim(miny-10,maxy+10)
+
+	plt.tight_layout()
+	savename = "jpyplot2D_N" + str(particles) + "_S" + str(steps) + ".png"
+	plt.savefig(savename)
+
+if __name__=="__main__":
+	main()
